0e5267d4ee064f92df25243bb28c496ec4b98157
Christopher Vazquez <cvg_v10@hotmail.com>
2022-05-08 21:06:33 -0700

    required connection.js and db sync

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 // import sequelize connection
-
+const db = require('./config/connection.js')
 const app = express()
 
 app.use(express.json())
@@ -8,5 +8,6 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(require('./routes'))
 
+db.sync()
 // sync sequelize models to the database, then turn on the server
 app.listen(process.env.PORT || 3000)
